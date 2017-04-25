433066fd5994fe67959fd6721270eac816d8623d
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-25 13:43:30 -0400

    add nuspec support

--- src/LagoVista.IoT.Logging/DefaultLogger.cs
+++ src/LagoVista.IoT.Logging/DefaultLogger.cs
@@ -9,7 +9,7 @@ namespace LagoVista.IoT.Logging
     {
         public void Log(LogLevel level, string area, string message, params KeyValuePair<string, string>[] args)
         {
-
+            
         }
 
         public void LogException(string area, Exception ex, params KeyValuePair<string, string>[] args)
