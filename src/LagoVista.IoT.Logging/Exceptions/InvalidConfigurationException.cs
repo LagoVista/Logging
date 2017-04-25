a7b746557182a9a019058869c79857a7043011d3
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-25 13:39:10 -0400

    add invalid configuration exception

---
+++ src/LagoVista.IoT.Logging/Exceptions/InvalidConfigurationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Logging.Exceptions
+{
+    public class InvalidConfigurationException : Exception
+    {
+        public InvalidConfigurationException(Error errorCode)
+        {
+            Error = errorCode;
+        }
+
+        public InvalidConfigurationException(Error errorCode, string details)
+        {
+            Error = errorCode;
+            Error.Details = details;
+        }
+
+        public InvalidConfigurationException(ErrorCode errorCode, string details)
+        {
+            Error = errorCode.ToError();
+            Error.Details = details;
+        }
+
+        public InvalidConfigurationException(ErrorCode errorCode, Type configurationType, string id, string details = "")
+        {
+            Error = errorCode.ToError();
+            Error.ConfigurationId = id;
+            Error.ConfiguratinType = configurationType.FullName;
+            Error.Details = details;
+        }
+
+        public Error Error { get; private set; }
+    }
+}
