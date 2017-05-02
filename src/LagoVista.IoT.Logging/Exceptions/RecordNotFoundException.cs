f84c6e8908493543f07d86541472d1f82a51c6ab
Kevin D. Wolf <kevinw@software-logistics.com>
2017-05-02 14:56:25 -0400

    NUGET Version 0.8.152-alpha1444

---
+++ src/LagoVista.IoT.Logging/Exceptions/RecordNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Logging.Exceptions
+{
+    public class RecordNotFoundException : Exception
+    {
+
+
+        public Error Error { get; private set; }
+
+        public string RecordType { get; private set; }
+
+        public string RecordId { get; private set; }
+
+        public static RecordNotFoundException FromErrorCode(ErrorCode errorCode, string recordType, string recordId)
+        {
+            return new RecordNotFoundException()
+            {
+                Error = errorCode.ToError(),
+                RecordType = recordType,
+                RecordId = recordId
+            };
+        }
+    }
+}
