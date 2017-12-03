4905ad0129b594d6d0638623bae0fa1789aa62a8
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-12-03 08:46:32 -0500

    tuning on pem storage

--- src/LagoVista.IoT.Logging/Error.cs
+++ src/LagoVista.IoT.Logging/Error.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,20 +8,38 @@ namespace LagoVista.IoT.Logging
 {
     public class Error
     {
-        public string InstanceHostId { get; set; }
+        [JsonProperty("hostId", NullValueHandling = NullValueHandling.Ignore)]
+        public string HostId { get; set; }
+
+        [JsonProperty("instanceId", NullValueHandling = NullValueHandling.Ignore)]
         public string InstanceId { get; set; }
-        public string DeploymentId { get; set; }
+
+        [JsonProperty("configurationType", NullValueHandling = NullValueHandling.Ignore)]
+        public string ConfigurationType { get; set; }
+
+        [JsonProperty("configurationId", NullValueHandling = NullValueHandling.Ignore)]
+        public string ConfigurationId { get; set; }
+
+
+        [JsonProperty("solutionId", NullValueHandling = NullValueHandling.Ignore)]
+        public string SolutionId { get; set; }
+        [JsonProperty("deviceConfigurationId", NullValueHandling = NullValueHandling.Ignore)]
         public string DeviceConfigurationId { get; set; }
 
-        public string ConfigurationType { get; set; }
-        public string ConfigurationId { get; set; }
+        [JsonProperty("deviceId", NullValueHandling = NullValueHandling.Ignore)]
         public string DeviceId { get; set; }
-
+        [JsonProperty("errorCode", NullValueHandling = NullValueHandling.Ignore)]
         public string ErrorCode { get; set; }
+        [JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
         public string Message { get; set; }
+        [JsonProperty("details", NullValueHandling = NullValueHandling.Ignore)]
         public string Details { get; set; }
+        [JsonProperty("extras", NullValueHandling = NullValueHandling.Ignore)]
         public List<KeyValuePair<string, string>> Extras { get; set; }
 
-
+        public void SetEmptyValueToNull()
+        {
+            if (Extras != null && !Extras.Any()) Extras = null;
+        }
     }
 }
