f99443985872cad9328798da518d7b7ab7432ed7
Kelvin McDaniel <kelvin.mcdaniel@software-logistics.com>
2021-01-14 09:17:41 -0500

    Missing file from last push.

--- src/LagoVista.IoT.Logging/Loggers/ILogReader.cs
+++ src/LagoVista.IoT.Logging/Loggers/ILogReader.cs
@@ -1,29 +1,27 @@
 ﻿using LagoVista.Core.Models.UIMetaData;
 using LagoVista.IoT.Logging.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LagoVista.IoT.Logging.Loggers
 {
-    public enum ResourceType
-    {
-        WebTools,
-        API,
-        Host,
-        Instance,
-        PipelineModule,
-        Device,
-        Any,        
-    }
+	public enum ResourceType
+	{
+		WebTools,
+		API,
+		Host,
+		Instance,
+		PipelineModule,
+		Device,
+		Any,
+		Scheduler,
+	}
 
-    public interface ILogReader
-    {
-        Task<ListResponse<LogRecord>> GetLogRecordsAsync(ResourceType resourceType, ListRequest listRequest);
-        Task<ListResponse<LogRecord>> GetErrorsAsync(ResourceType resourceType, ListRequest listRequest);
-        Task<ListResponse<LogRecord>> GetLogRecordsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
-        Task<ListResponse<LogRecord>> GetErrorsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
-        Task<ListResponse<LogRecord>> GetAllErrorsAsync(ListRequest listRequest);
-    }
+	public interface ILogReader
+	{
+		Task<ListResponse<LogRecord>> GetLogRecordsAsync(ResourceType resourceType, ListRequest listRequest);
+		Task<ListResponse<LogRecord>> GetErrorsAsync(ResourceType resourceType, ListRequest listRequest);
+		Task<ListResponse<LogRecord>> GetLogRecordsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
+		Task<ListResponse<LogRecord>> GetErrorsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
+		Task<ListResponse<LogRecord>> GetAllErrorsAsync(ListRequest listRequest);
+	}
 }
