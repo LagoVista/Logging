bc90276f1344f430162549ba8b60a713e27c7418
Kevin D. Wolf <kevinw@software-logistics.com>
2024-04-22 12:35:59 -0400

    update to netstandard 2.1, add log writer

---
+++ src/LagoVista.IoT.Logging/Utils/LogWriter.cs
@@ -0,0 +1,32 @@
+﻿using LagoVista.IoT.Logging.Loggers;
+using LagoVista.IoT.Logging.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.Logging.Utils
+{
+    public class ConsoleLogWriter : ILogWriter
+    {
+        public Task WriteError(LogRecord record)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine($"[error] {DateTime.Now.ToShortTimeString()} {record.Area} - {record.Message}");
+            if (!String.IsNullOrEmpty(record.Details))
+                Console.WriteLine(record.Details);
+
+            Console.ResetColor();
+
+            return Task.CompletedTask;
+        }
+
+        public Task WriteEvent(LogRecord record)
+        {
+            Console.WriteLine($"[event] {DateTime.Now.ToShortTimeString()} {record.Area} - {record.Message}");
+            if (!String.IsNullOrEmpty(record.Details))
+                Console.WriteLine(record.Details);            
+
+            return Task.CompletedTask;
+        }
+    }
+}
