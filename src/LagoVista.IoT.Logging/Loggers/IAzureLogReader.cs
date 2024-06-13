1091cf6113b71592f48c342ae6cb7d1cef62060b
Kevin D. Wolf <kevinw@software-logistics.com>
2024-06-13 06:44:35 -0400

    add interface for azure log reader for instance logging

---
+++ src/LagoVista.IoT.Logging/Loggers/IAzureLogReader.cs
@@ -0,0 +1,19 @@
+﻿using LagoVista.Core.Models.UIMetaData;
+using LagoVista.IoT.Logging.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.Logging.Loggers
+{
+    public interface IAzureLogReader
+    {
+        Task<ListResponse<LogRecord>> GetLogRecordsAsync(ResourceType resourceType, ListRequest listRequest);
+        Task<ListResponse<LogRecord>> GetErrorsAsync(ResourceType resourceType, ListRequest listRequest);
+        Task<ListResponse<LogRecord>> GetLogRecordsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
+        Task<ListResponse<LogRecord>> GetErrorsAsync(string resourceId, ListRequest listRequest, ResourceType resourceType = ResourceType.Any);
+        Task<ListResponse<LogRecord>> GetAllErrorsAsync(ListRequest listRequest);
+
+    }
+}
